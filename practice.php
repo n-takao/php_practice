a5227828a28dd3a83423b56747aed2689b11a95a
n-takao <ren615myu072@gmail.com>
2020-07-22 06:47:46 +0000

    php課題の答え

---
+++ practice.php
@@ -0,0 +1,41 @@
+<?php
+
+$a=3;
+$b=7;
+echo $a+$b;
+echo "\n";
+
+$array_manth=["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_manth[7];
+echo "\n";
+
+$hello="Hello,";
+$name="takao";
+$world="'World!";
+echo $hello.$name.$world;
+
+echo "\n";
+
+$tech_boost="tech";
+$tech_boost .="_boost";
+echo $tech_boost;
+echo "\n";
+
+$tech_boost2018_calendder = [
+ "January"=>"1月",
+ "february"=>"2月",
+ "march"=>"3月",
+ "April"=>"4月",
+ "May"=>"5月",
+ "June"=>"6月",
+ "July"=>"7月",
+ "August"=>"8月",
+ "September"=>"9月",
+ "October"=>"10月",
+ "November"=>"11月",
+ "December"=>"12月"
+ ];
+ echo $tech_boost2018_calendder["December"];
+ echo "\n";
+    
+    
